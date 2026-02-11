9e8281304559dec7bd0d37489163cf3a1beea264
User <User@LAPTOP-7KK2OI41>
2026-02-11 01:21:54 -0500

    Add project files.

---
+++ VerificarPar/Program.cs
@@ -0,0 +1,31 @@
+﻿var numberString = String.Empty;
+do
+{
+    Console.Write("Ingrese numero entero o la palabra 'Salir' para salir: ");
+    numberString = Console.ReadLine();
+    if (numberString!.ToLower() == "salir")
+    {
+        continue;
+    }
+
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if(numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El Numero {numberInt} es par");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt} es impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Lo que ingresaste: {numberString}, no es un numero entero");
+    }
+    
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Game Over, ");
+
+
