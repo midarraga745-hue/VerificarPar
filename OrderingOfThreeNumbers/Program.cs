1aedc6e78a38b97d84893d64e9a52f41ab4319c2
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-02-12 00:58:28 -0500

    OrderingOfThreeNumbers

---
+++ OrderingOfThreeNumbers/Program.cs
@@ -0,0 +1,61 @@
+﻿using Shared;
+
+do
+{
+    Console.WriteLine("Ingrese 3 numeros diferentes...");
+    var a = ConsoleExtension.GetInt("Ingrese primer numero : ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo numero: ");
+    if (a == b)
+    {
+        Console.WriteLine("Deben ser diferentes, vuelva a empezar");
+        continue;
+    }
+
+    var c = ConsoleExtension.GetInt("Ingrese tercero numero: ");
+    if (b == c || c == a)
+    {
+        Console.WriteLine("Deben ser diferentes vuelva a empezar...");
+        continue;
+    }
+
+    if (a > b && a > c)
+    {
+        if (b > c)
+        {
+            Console.WriteLine($"El mayor es {a}, el medio es {b}, el menor es {c}");
+        }
+        else
+        {
+            Console.WriteLine($"El mayor es {a}, el medio es {c}, el menor es {b}");
+        }
+    }
+    else if (b > a && b > c)
+    {
+        if (a > c)
+        {
+            Console.WriteLine($"El mayor es {b}, el medio es {a}, el menor es {c}");
+        }
+        else
+        {
+            Console.WriteLine($"El mayor es {b}, el medio es {c}, el menor es {a}");
+        }
+    }
+    else
+    {
+        if (a > b)
+        {
+            Console.WriteLine($"El mayor es {c}, el medio es {a}, el menor es {b}");
+        }
+        else
+        {
+            Console.WriteLine($"El mayor es {c}, el medio es {b}, el menor es");
+        }
+    
+    }
+
+       
+
+} while (true);
+
+
+        
