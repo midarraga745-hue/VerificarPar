7936712431d78b8e34dda73c7cffa7809bc55d35
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-02-12 22:17:15 -0500

    Is Leap Year

---
+++ IsLeapYear/Program.cs
@@ -0,0 +1,44 @@
+﻿using Shared;
+
+do
+{
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtension.GetInt("Ingrese año:");
+
+    if (year == currentYear)
+    {
+        message = "es";
+    } else if(year > currentYear)
+    {
+        message = "va a ser";
+    }
+    else
+    {
+        message = "fue";
+    }
+
+
+   if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($"El año: {year}, SI es bisiesto.");
+            }
+            else
+            {
+                Console.WriteLine($"El año: {year}, No es bisiesto.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"El año: {year}, SI es bisiesto.");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El año: {year}, No es bisiesto.");
+    }
+} while (true);
